2056cee848823fbd84f92b5eb6e5aff1b5ee38e7
Braden Farmer <farmerbb@gmail.com>
2024-08-30 13:45:45 -0600

    Updating dotfiles

--- Bash Configs/bashrc-additions-nuc.sh
+++ Bash Configs/bashrc-additions-nuc.sh
@@ -2,6 +2,7 @@ export LINUX_DIR_PREFIX="$(realpath /mnt/files/Other\ Stuff/Linux)"
 export DEVICE_DIR_PREFIX="$(realpath /mnt/files/Other\ Stuff/Linux/Devices/NUC)"
 export OD_LINUX_DIR_PREFIX="/home/$USER/OneDrive/Other Stuff/Linux"
 export OD_DEVICE_DIR_PREFIX="/home/$USER/OneDrive/Other Stuff/Linux/Devices/NUC"
+export BTRFS_MNT="/mnt/VMs"
 export PATH="$PATH:$LINUX_DIR_PREFIX/Scripts"
 
 export SYNC_DIRS=(
--- Bash Configs/bashrc-additions-xps13.sh
+++ Bash Configs/bashrc-additions-xps13.sh
@@ -2,8 +2,7 @@ export LINUX_DIR_PREFIX="/home/$USER/Other Stuff/Linux"
 export DEVICE_DIR_PREFIX="/home/$USER/Other Stuff/Linux/Devices/Dell XPS 13"
 export OD_LINUX_DIR_PREFIX="/home/$USER/OneDrive/Other Stuff/Linux"
 export OD_DEVICE_DIR_PREFIX="/home/$USER/OneDrive/Other Stuff/Linux/Devices/Dell XPS 13"
-# export BTRFS_MNT="/mnt/files"
-# export BTRFS_HOME_MNT="/mnt/files/Local/Home"
+export BTRFS_MNT="/mnt/VMs"
 export PATH="$PATH:$LINUX_DIR_PREFIX/Scripts"
 
 export SYNC_DIRS=(
